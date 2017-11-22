b575256ebcf1d49b0687239266f6a954eaf57aaf
jyschwrtz <jyschwrtz@gmail.com>
2017-11-21 18:06:48 -0800

    in progress - splash, login, and signup page styling

--- spotify/frontend/components/greeting/greeting.jsx
+++ spotify/frontend/components/greeting/greeting.jsx
@@ -5,13 +5,27 @@ export default () => {
 
   return (
     <div className="splash">
-      <h1>Spotify</h1>
-      <button>
-        <Link to='/signup'>Sign Up</Link>
-      </button>
-      <button>
-        <Link to='/login'>Log In</Link>
-      </button>
+      <div className="splash-content">
+        <div className="splash-left">
+          <h1>Spotify</h1>
+          <button className="splash-signup">
+            <Link to='/signup'>SIGN UP</Link>
+          </button>
+          <h2>ALREADY HAVE AN ACCOUNT?</h2>
+          <button className="splash-login">
+            <Link to='/login'>LOG IN</Link>
+          </button>
+        </div>
+        <div className="splash-right">
+          <h1>Get the right music,</h1>
+          <h1>right now</h1>
+          <h3>Listen to millions of songs for free.</h3>
+          <ul>
+            <li>Search & discover music you'll love</li>
+            <li>Create playlists of your favorite music</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
