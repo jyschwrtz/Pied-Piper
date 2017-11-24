8aca3332970cacf69ad0ba1dda49ab84c386f1c3
jyschwrtz <jyschwrtz@gmail.com>
2017-11-24 11:18:03 -0800

    music-player CSS styling setup

--- frontend/components/control_bar/control_bar.jsx
+++ frontend/components/control_bar/control_bar.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import ReactHowler from 'react-howler';
 
 class ControlBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false
+    };
+  }
+
+  togglePlay() {
+    const playing = this.state.playing === true ? false : true;
+    this.setState({playing});
+  }
 
   render() {
+    const { playing } = this.state;
     return (
       <div className="control-bar">
         <h1>Control Bar</h1>
+
+        <ReactHowler
+          src={['https://s3-us-west-1.amazonaws.com/pied-piper-spotify-clone/Music/Singles/Melanie_Ungar_-_Crazy_Glue.mp3', 'https://s3-us-west-1.amazonaws.com/pied-piper-spotify-clone/Music/Singles/Craze_24_-_Rolling__Original_Clean_.mp3']}
+          playing={playing}
+          volume={1}
+          html5={true}
+        />
+        <button
+          onClick={this.togglePlay.bind(this)}
+          >Play/Pause</button>
       </div>
     );
   }
--- frontend/components/sidebar/sidebar.jsx
+++ frontend/components/sidebar/sidebar.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import ReactHowler from 'react-howler';
 
 class Sidebar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      playing: false
-    };
-  }
 
-  togglePlay() {
-    const playing = this.state.playing === true ? false : true;
-    this.setState({playing});
   }
 
   render() {
-    const { playing } = this.state;
+
     return(
       <div className="sidebar">
         <h1>Sidebar</h1>
@@ -24,17 +16,6 @@ class Sidebar extends React.Component {
           className="logout-btn"
           onClick={this.props.logout}
           >Log Out</button>
-
-          <ReactHowler
-            src={['https://s3-us-west-1.amazonaws.com/pied-piper-spotify-clone/Music/Singles/Melanie_Ungar_-_Crazy_Glue.mp3', 'https://s3-us-west-1.amazonaws.com/pied-piper-spotify-clone/Music/Singles/Craze_24_-_Rolling__Original_Clean_.mp3']}
-            playing={playing}
-            volume={1}
-            html5={true}
-          />
-        <button
-          onClick={this.togglePlay.bind(this)}
-          >Play/Pause</button>
-
       </div>
     );
   }
